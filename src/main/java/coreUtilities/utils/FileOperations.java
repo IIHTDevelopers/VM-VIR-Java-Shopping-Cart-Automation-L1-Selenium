90d961b97fcc9f698b7455dd885c9e4539892665
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-05-30 11:01:56 +0530

    Update FileOperations.java

--- src/main/java/coreUtilities/utils/FileOperations.java
+++ src/main/java/coreUtilities/utils/FileOperations.java
@@ -33,7 +33,9 @@ public class FileOperations
 	public Map<String, String> readJson(String file, String jsonNode) throws FileNotFoundException, IOException, ParseException
 	{
 		
-		return null;
+		jsonParser = new JSONParser();
+		jsonObject = (JSONObject)jsonParser.parse(new FileReader(file));
+        	return (Map<String, String>) jsonObject.get(jsonNode);
 	}
 	
 }
